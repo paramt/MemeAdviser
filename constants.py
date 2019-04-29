4ef1fb36d98ee61137e21ab5e6fded384a5f8581
Param Thakkar <pht0104@gmail.com>
2019-04-28 22:39:21 -0400

    Fixed formatting of Messages
    Changed multi-line string to a single-line string with newline characters. Multi-line strings included tabs, which were interpreted as code blocks by Reddit's markdown.

--- constants.py
+++ constants.py
@@ -5,20 +5,6 @@ class Thresholds:
 
 
 class Messages:
-    comment = '''I **strongly advise** investing! This meme hit #1 on [hot](https://www.reddit.com/r/memeeconomy/hot/) within **{min}**, at **{upvotes}** upvotes. If you invest now, you'll break even at **{break_even}** upvotes.
-
-    [Click here](https://www.param.me/meme/calculator/break-even) to calculate the current break-even point. [Click here](https://www.reddit.com/message/compose?to=MemeAdviser&subject=Subscribe&message=Subscribe) to subscribe to daily market updates.
-    
-    *****
-    
-    ^(Beep boop, I'm a bot | [Contact me](mailto://bot@param.me))
-    '''
-
+    comment = "I **strongly advise** investing! This meme hit #1 on [hot](https://www.reddit.com/r/memeeconomy/hot/) within **{min}**, at **{upvotes}** upvotes. If you invest now, you'll break even at **{break_even}** upvotes.\n\n[Click here](https://www.param.me/meme/calculator/break-even) to calculate the current break-even point. [Click here](https://www.reddit.com/message/compose?to=MemeAdviser&subject=Subscribe&message=Subscribe) to subscribe to daily market updates.\n***\n^(Beep boop, I'm a bot | [Contact me](mailto://bot@param.me))"
     submission = "This meme just hit #1 on MemeEconomy with only {upvotes} upvotes! Invest now and break even at {break_even} upvotes"
-
-    pm = '''[This meme](https://reddit.com{link}) just hit #1 on MemeEconomy with only {upvotes} upvotes! Invest now and break even at {break_even} upvotes
-
-    ***
-    
-    ^(You're recieving this message because you've subscribed to this bot. To unsubscribe, reply 'Unsubscribe')
-    '''
+    pm = "[This meme](https://reddit.com{link}) just hit #1 on MemeEconomy with only {upvotes} upvotes! Invest now and break even at {break_even} upvotes\n***\n^(You're recieving this message because you've subscribed to this bot. To unsubscribe, reply 'Unsubscribe')"
