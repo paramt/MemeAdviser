3bc369127fe7213fe3c3fec53c465994fff337a1
Param Thakkar <pht0104@gmail.com>
2019-05-23 16:38:34 -0400

    Add color highlighting to /docs/log
    Critical: red
    Error: orange
    Warning: yellow
    Info: white
    Debug: gray

--- docs/log/script.js
+++ docs/log/script.js
@@ -1,8 +1,50 @@
+red = "<span style='color: red'>";
+orange = "<span style='color: orange'>";
+yellow = "<span style='color: yellow'>";
+white = "<span style='color: white'>";
+gray = "<span style='color: gray'>";
+
+coloredEntries = []
+
+function highlightText(log){
+  log = log.split("\n");
+
+  log.forEach(function(entry){
+    if(entry.includes("CRITICAL: ")){
+      coloredEntry = red + entry + "</span>";
+    }
+
+    if(entry.includes("ERROR: ")){
+      coloredEntry = orange + entry + "</span>";
+    }
+
+    if(entry.includes("WARNING: ")){
+      coloredEntry = yellow + entry + "</span>";
+    }
+
+    if(entry.includes("INFO: ")){
+      coloredEntry = white + entry + "</span>";
+    }
+
+    if(entry.includes("DEBUG: ")){
+      coloredEntry = gray + entry + "</span>";
+    }
+
+    if(entry.includes("WARNING: ")){
+      coloredEntry = yellow + entry + "</span>";
+    }
+
+    coloredEntries.push(coloredEntry);
+    document.getElementById("terminal").innerHTML = coloredEntries.join("\n");
+  });
+}
+
 var xhttp=new XMLHttpRequest();
 
 xhttp.onreadystatechange=function(){
   if(this.readyState==4&&this.status==200){
     document.getElementById("terminal").innerHTML = this.responseText;
+    highlightText(this.responseText)
   }
 };
 
