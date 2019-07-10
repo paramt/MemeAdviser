21d93f6505634c97c857eb1ac33aa315f4fd159c
Param Thakkar <pht0104@gmail.com>
2019-07-10 12:30:49 -0400

    Update run.sh to reflect changes to crontab
    Crontab was changed in late May to address #17 but run.sh was never updated (this commit doesn't change anything on the server)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/param/meme/MemeAdviser4/MemeAdviser
 git pull
-python3 -m src.app >> /home/param/meme/MemeAdviser4/cron.log 2>&1
+run-one python3 -m src.app >> /home/param/meme/MemeAdviser4/cron.log 2>&1
