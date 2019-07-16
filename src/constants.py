e88d2eb407725ec872f661a311c3fd7f24a61726
Param Thakkar <pht0104@gmail.com>
2019-07-16 09:13:01 -0400

    Remove submission constants

--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,10 @@
 class Thresholds:
-	submission = 600
 	comment = 800
 	pm = 800
 
 
 class Messages:
 	comment = "I **strongly advise** investing! This meme hit #1 on [hot](https://www.reddit.com/r/memeeconomy/hot/) within **{time}**, at **{upvotes}** upvotes. If you invest now, you'll break even at **{break_even}** upvotes.\n\n[Click here](https://www.param.me/meme/calculator/break-even) to calculate the current break-even point. [Click here](https://www.reddit.com/message/compose?to=MemeAdviser&subject=Subscribe&message=Subscribe) to subscribe to daily market updates.\n***\n^(I'm a bot | [contact](https://www.reddit.com/message/compose?to=hypnotic-hippo&subject=MemeAdviser) | [source](https://github.com/paramt/MemeAdviser) | [website](https://www.param.me/MemeAdviser))"
-	submission = "This meme just hit #1 on MemeEconomy with only {upvotes} upvotes! Invest now and break even at {break_even} upvotes"
 	pm = "[This meme](https://reddit.com{link}) just hit #1 on MemeEconomy with only {upvotes} upvotes! Invest now and break even at {break_even} upvotes\n***\n^(You're recieving this message because you've subscribed to this bot. To unsubscribe, reply 'Unsubscribe')"
 
 
